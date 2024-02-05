b890a9bf48ef4ab218553f04b03b3011c627fd96
Ethan k <ethan7klukkert@gmail.com>
2024-02-05 16:00:07 -0500

    Add placeholder landing page for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
 import Link from "next/link";
+import Button from "./ui/button";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen p-24">
-      <h1 className="block">
-        Project Management Application
-      </h1>
-      <div>By Ethan Klukkert</div>
-      <div className="flex flex-col">
-        <div>
-          <button className="bg-sky-300 rounded p-2 m-2 hover:bg-sky-400"><Link href={"/login"}>Login</Link></button>
-          <button className="bg-sky-300 rounded p-2 m-2 hover:bg-sky-400"><Link href={"/dashboard"}>Home</Link></button>
+  return <>
+    <header className="p-12 text-4xl"><h1 className="block">
+      Logis Projects
+    </h1></header>
+    <main className="p-2">
+      <section className="min-h-screen" id="intro">
+        <h1 className="text-3xl text-center">
+          The #1 Project Management Software For <br /><strong className="text-blue-500">On-Site Contractors</strong>
+        </h1>
+        <div className="flex flex-col mt-4">
+          <Button className=""><Link href={"/login"} className=" font-bold">Login</Link></Button>
+          <Button className=""><Link href={"/login"} className=" font-bold">Sign Up</Link></Button>
+          <Image src={"/preview-placeholder.png"} alt="Logis Projects Dashboard Preview" width={600} height={600} className="rounded mt-4"></Image>
         </div>
 
+      </section>
 
-      </div>
+      <section className="min-h-screen">
+        <h1 className="text-3xl text-center mb-16">The Best App For <strong className="text-blue-500">All Trades</strong>.</h1>
+        <p className="text-xl">Oversee work site status and tasks.</p>
+        <Image src={"/preview-placeholder.png"} alt="Logis Projects Dashboard Preview GIF" width={600} height={600} className="rounded mt-4 mb-16"></Image>
+        <p className="text-xl">Manage project tasks, inspections, estimates, and more.</p>
+        <Image src={"/preview-placeholder.png"} alt="Logis Projects Dashboard Preview GIF" width={600} height={600} className="rounded mt-4 mb-16"></Image>
+      </section>
+
+      <section className="min-h-screen grid place-content-center" id="cta">
+        <h1 className="text-xl text-center">Landscape, Electricty, Plumbing, Construction, HVAC, and everything else involving hands-on projects. If you're on-site, <strong className="text-blue-500">you need our site</strong>!</h1>
+        <div className="flex flex-col mt-4">
+          <Button className=""><Link href={"/login"} className=" font-bold">Try Free</Link></Button>
+        </div>
+      </section>
+
+    </main></>
 
 
-    </main>
-  );
 }
---
+++ app/ui/button.tsx
@@ -0,0 +1,11 @@
+import { inter } from "./fonts"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function Button({ children, className, ...rest }: ButtonProps) {
+  return <button {...rest} className={`${className} ${inter.className} bg-blue-500 rounded-full py-2 px-4 m-2 hover:bg-blue-300 text-white`}>
+    {children}
+  </button>
+}
