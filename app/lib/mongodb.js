23205c0a531eac29473c8d1c001a5ede81b45934
Ethan k <ethan7klukkert@gmail.com>
2024-03-05 17:01:08 -0500

    Fix mongoose.connect to call only once

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,6 +1,22 @@
 import mongoose from "mongoose";
 
 export default async function connectMongoDB() {
+  const readyState = mongoose.connection.readyState;
+  switch (readyState) {
+    case 0:
+      console.log("Not connected to db. Will connect to db...")
+      break;
+    case 1:
+      console.log("Already Connected to db! Preventing another connection call...")
+      return;  // Prevents multiple calls to mongoose.connect (one connection per app is best practice)
+    case 2:
+      console.log("Connecting to db...")
+      break;
+    case 3:
+      console.log("Disconnecting from db...")
+      break;
+  }
+
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB")
